fc948c34a19a9e210fcffc61588d35f8b77e8f3a
jlmbanana <jessica.l.martin@odot.oregon.gov>
2025-02-05 11:07:23 -0800

    Update streamlit_app.py
    Pull the SEARCH_ON column from fruit_option table into the df
    Look at results

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,12 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
 
 #Uncomment below line to see the dataframe:
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+                                                                      
 #Put a multiselect widget on app
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',

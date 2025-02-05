ad0736d85a959be03b88126e2aca170652feffdd
jlmbanana <jessica.l.martin@odot.oregon.gov>
2025-02-05 09:55:24 -0800

    Update streamlit_app.py
    Put json info from API into df object

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@
 import streamlit as st
 import requests  #NEW FOR API CONNECT
 
-###API: get info from the smoothiefroot website
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
 
 from snowflake.snowpark.functions import col, when_matched
 #Commented out for SniS---
@@ -66,3 +64,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
+###API: get info from the smoothiefroot website
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+#Put json info into df object:
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
